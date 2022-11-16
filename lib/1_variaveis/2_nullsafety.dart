deaf5d1563a11e5be72e5a072d1aec9ce702e11a
Francisco <fsousa.jpa@gmail.com>
2022-11-16 16:50:09 -0300

    add modificadores

--- lib/1_variaveis/2_nullsafety.dart
+++ lib/1_variaveis/2_nullsafety.dart
@@ -1,8 +1,41 @@
+// variaveis de nivel superior nao podem ser inicializadas depois
+// nao é permitido
+// String nomeSuperior;
+
+String nomeSuperior = 'Francisco';
+String? nomeSuperiorOpc;
+
 void main() {
-  String nome = 'Francisco';
+  // variaveis locais nao precisam ser inicializadas de cara porem
+  // podem  ser inicializadas sem valor e depois ser atribuido um valor
+  String nomeCompleto;
+  String? nomeCompletoOpc;
+  // String? - Informa que a variavel pode ser nula
+  // String? nomeCompleto = 'Francisco Sousa'; // variavel nao e mais nula
+  nomeCompleto = 'Francisco Sousa';
 
-  int numero = 20;
-  int numero1 = 20;
+  // variaveis locais que sao nulas(nullable) por padrao
+  // se for atribuido um valor a ela, automaticamente elas sao
+  // promovidas a nao nulo (non-null)
+  nomeCompletoOpc = '';
 
-  print(nome);
+  // variaveis de nivel superior NUNCA sao promovidas para nao nulo(non-null)
+  nomeSuperiorOpc = '';
+
+  print(nomeCompleto.length);
+  print(nomeSuperior.length);
+
+/* 
+  //Tratamento null safety
+  String? nomeCompleto;
+  if (nomeCompleto != null) {
+    print(nomeCompleto.length);
+  } else {
+    print('É nulo!');
+  }
+ */
+  // String? nomeCompleto; //* ? String aceita nulo
+
+  print(nomeCompleto
+      .length); //* ! força compilador a aceitar nulo (vai da erro de compilacao nesse caso)
 }
---
+++ lib/2_modificadores/1_modificadores.dart
@@ -0,0 +1,13 @@
+void main() {
+  String nomeCompleto = 'Franicsco';
+  print(nomeCompleto);
+  nomeCompleto = 'Francisco Sousa';
+
+  // final
+  // variavel se torna imutavel
+  final nomeCompletoFinal = 'Francisco Sousa';
+  // nomeCompletoFinal = 'Francisco Sousa'; //* nao funciona
+
+  // const
+  
+}
