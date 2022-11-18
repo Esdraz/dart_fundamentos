360f9e9178ab93ee9368798d29569a479da3ad3f
Francisco <fsousa.jpa@gmail.com>
2022-11-17 21:34:16 -0300

    add modificadores

--- lib/2_modificadores/1_modificadores.dart
+++ lib/2_modificadores/1_modificadores.dart
@@ -4,10 +4,22 @@ void main() {
   nomeCompleto = 'Francisco Sousa';
 
   // final
-  // variavel se torna imutavel
+  // nao podem ser alteradas depois de inicializadas (imutaveis)
+  // sao definidas em tempo de execuçao (runtime)
   final nomeCompletoFinal = 'Francisco Sousa';
   // nomeCompletoFinal = 'Francisco Sousa'; //* nao funciona
 
+  // dessa forma abaixo funciona:
+  // final nomeCompletoFinal1;
+  // nomeCompletoFinal1 = 'Jose Joao';
+
   // const
-  
+  // nao podem ser alteradas depois de inicializadas (imutaveis)
+  // sao definidas no programa em tempo de COMPILACAO
+  // so recebe valores de outras constantes
+  // desta forma nao funciona:
+  // const nomeCompletoConst2 = nomeCompleto;
+
+  const nomeCompletoConst = 'Francisco Sousa';
+  const nomeCompletoConst1 = nomeCompletoConst;
 }
